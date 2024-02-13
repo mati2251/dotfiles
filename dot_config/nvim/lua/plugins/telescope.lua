10629cfc2e70cdb262154915f540ce3038d849aa
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2024-02-13 21:52:19 +0100

    Added telescope new keybind

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.5',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
@@ -7,6 +8,18 @@ return {
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
+    local actions = require('telescope.actions')
+    require('telescope').setup {
+      defaults = {
+        mappings = {
+          n = {
+            ['<C-d>'] = actions.delete_buffer
+          },
+          i = {
+            ['<C-d>'] = actions.delete_buffer
+          },
+        },
+      },
+    }
   end
 }
-
