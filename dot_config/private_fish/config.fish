d7b44f5c47045b4914ce72309875253b05d385a1
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2024-04-02 17:59:49 +0200

    Improved fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,10 @@
 # editor set
 set -gx EDITOR nvim
-
+set fish_key_bindings fish_vi_key_bindings
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/mateusz/.ghcup/bin $PATH # ghcup-env
+
+zoxide init fish --cmd cd | source
